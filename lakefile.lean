9661f239ffdf88260b6735ffe1cc191274aca8f0
Jeremy Toh <7417963+deepimpactmir@users.noreply.github.com>
2024-02-13 08:26:00 +0800

    add c03_logic/s01

--- lakefile.lean
+++ lakefile.lean
@@ -11,4 +11,5 @@ package mil where
 lean_lib MIL where
 
 require mathlib from git "https://github.com/leanprover-community/mathlib4"@"master"
-
+require LeanCopilot from git "https://github.com/lean-dojo/LeanCopilot.git" @ "v1.1.1"
+require Duper from git "https://github.com/leanprover-community/duper.git" @ "v0.0.8"
